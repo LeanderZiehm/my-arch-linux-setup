413014c33c2b9d4695092e59017d25dfb8d547f5
LeanderZiehm <leander-arun.zieh@stud.th-deg.de>
2025-10-16 23:56:25 +0200

    update: changed cli/gitacp.sh

--- cli/gitacp.sh
+++ cli/gitacp.sh
@@ -1,3 +1,9 @@
 git add .
-git commit -m "+"
+files=$(git diff --cached --name-only)
+if [ -z "$files" ]; then
+  echo "No changes to commit."
+  return
+fi
+msg="update: changed $(echo $files | tr '\n' ' ' | sed 's/ $//')"
+git commit -m "$msg"
 git push
