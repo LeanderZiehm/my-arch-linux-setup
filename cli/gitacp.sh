5d9c73cf666a2c9b7a25fa6922197a082643c7f2
LeanderZiehm <leander-arun.ziehm@stud.th-deg.de>
2025-10-25 19:32:01 +0200

    changed: cli/gitacp.sh

--- cli/gitacp.sh
+++ cli/gitacp.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+# Find the root of the git repository
+git_root=$(git rev-parse --show-toplevel 2>/dev/null || true)
+
+if [ -z "$git_root" ]; then
+  echo "This is not a git repository."
+  exit 1
+fi
+
+# Move to the git root
+cd "$git_root"
+
 git add .
 
 # Get staged changes (status + filename)
@@ -19,9 +30,9 @@ deleted=()
 # Categorize files
 while IFS=$'\t' read -r status file; do
   case "$status" in
-  A) added+=("$file") ;;
-  M) changed+=("$file") ;;
-  D) deleted+=("$file") ;;
+    A) added+=("$file") ;;
+    M) changed+=("$file") ;;
+    D) deleted+=("$file") ;;
   esac
 done <<<"$diff_output"
 
